07c8110adbfc5f2be8fae3a67a729f83a33f77cf
DESKTOP-725RJQK\Vlad <vlad.vasian96@gmail.com>
2022-03-11 20:27:14 +0200

    v3

--- src/pages/contactus/contactus.jsx
+++ src/pages/contactus/contactus.jsx
@@ -148,6 +148,7 @@ const Contact = () => {
                   variant="outlined"
                   className="form-button"
                   disabled={isDisabled}
+                  type="submit"
                 >
                   Submit
                 </Button>
